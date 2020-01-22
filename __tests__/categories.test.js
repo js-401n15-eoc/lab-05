381a73d63f6a1682d31a8c88ea85653fcda412e9
Earl Jay Caoile <unfie555@gmail.com>
2020-01-22 01:22:29 -0800

    working tests for both categories and products

---
+++ __tests__/categories.test.js
@@ -0,0 +1,93 @@
+const Categories = require('../models/categories/categories.js');
+const supergoose = require('@code-fellows/supergoose');
+
+describe('Categories Collection', () => {
+
+  let categories;
+  let obj;
+  let badObj;
+  let fakeId;
+
+  beforeEach(() => {
+    categories = new Categories();
+    obj = {
+      name: 'Test Category 1',
+    };
+  
+    obj2 = {
+      name: 'Test Category 2',
+    };
+
+    editObj = {
+      name: 'Edited Test Category',
+    };
+
+    // badObj = {
+    //   category_id: 5555,
+    //   price: 'too much',
+    //   weight: '1 lb',
+    //   quantity_in_stock: 10,
+    // };
+
+    // fakeId = 'asdfasldkj31412341234';
+  });
+
+  it('can create() a new category', () => {
+    return categories.create(obj)
+    .then(data =>{
+      Object.keys(obj).forEach(key => {
+        expect(data[key]).toEqual(obj[key]);
+      });
+    }).catch(e => console.error('ERR', e));
+  });
+
+  it('can get() a category', () => {
+    return categories.create(obj)
+      .then(createData => {
+        return categories.get(createData.id)
+          .then(getData => {
+            Object.keys(obj).forEach(key => {
+              expect(getData[key]).toEqual(obj[key]);
+            });
+          });
+      }).catch(e => console.error('ERR', e));
+  });
+
+  it('can update() a category', () => {
+    return categories.create(obj)
+      .then(createData => {
+        return categories.get(createData.id)
+          .then(getData => {
+            return categories.update(getData.id, editObj)
+              .then(updateData => {
+                Object.keys(editObj).forEach(key => {
+                  expect(editObj[key]).toEqual(updateData[key]);
+                });
+              });
+          });
+      }).catch(e => console.error('ERR', e));
+  });
+
+  it('can delete() a category', () => {
+    return categories.create(obj)
+    .then(() => {
+      return categories.create(obj2)
+      .then(createData => {
+        return categories.get(createData.id)
+          .then(getData => {
+            return categories.update(getData.id, editObj)
+              .then(updateData => {
+                return categories.delete(updateData.id)
+                  .then(deletedData => {
+                    return categories.get(deletedData.id).
+                      then(emptyData => {
+                        expect(!!deletedData.id).toEqual(true);
+                        expect(emptyData).toEqual(null);
+                      })
+                    });
+                  });
+              });
+          });
+      }).catch(e => console.error('ERR', e));
+  });
+});
---
+++ __tests__/products.test.js
@@ -0,0 +1,102 @@
+const Products = require('../models/products/products.js');
+const supergoose = require('@code-fellows/supergoose');
+
+describe('Products Collection', () => {
+
+  let products;
+  let obj;
+  let badObj;
+  let fakeId;
+
+  beforeEach(() => {
+    products = new Products();
+    obj = {
+      category_id: '5555',
+      price: 444,
+      weight: 0.5,
+      quantity_in_stock: 10,
+    };
+  
+    obj2 = {
+      category_id: '777',
+      price: 711,
+      weight: 7,
+      quantity_in_stock: 77,
+    };
+
+    editObj = {
+      category_id: '777',
+      price: 999,
+      weight: 7,
+      quantity_in_stock: 999,
+    }
+
+    badObj = {
+      category_id: 5555,
+      price: 'too much',
+      weight: '1 lb',
+      quantity_in_stock: 10,
+    };
+
+    fakeId = 'asdfasldkj31412341234';
+  });
+
+  it('can create() a new product', () => {
+    return products.create(obj)
+    .then(data =>{
+      Object.keys(obj).forEach(key => {
+        expect(data[key]).toEqual(obj[key]);
+      });
+    }).catch(e => console.error('ERR', e));
+  });
+
+  it('can get() a product', () => {
+    return products.create(obj)
+      .then(createData => {
+        return products.get(createData.id)
+          .then(getData => {
+            Object.keys(obj).forEach(key => {
+              expect(getData[key]).toEqual(obj[key]);
+            });
+          });
+      }).catch(e => console.error('ERR', e));
+  });
+
+  it('can update() a product', () => {
+    return products.create(obj)
+      .then(createData => {
+        return products.get(createData.id)
+          .then(getData => {
+            return products.update(getData.id, editObj)
+              .then(updateData => {
+                Object.keys(editObj).forEach(key => {
+                  expect(editObj[key]).toEqual(updateData[key]);
+                });
+              });
+          });
+      }).catch(e => console.error('ERR', e));
+  });
+
+  it('can delete() a product', () => {
+    return products.create(obj)
+    .then(() => {
+      return products.create(obj2)
+      .then(createData => {
+        return products.get(createData.id)
+          .then(getData => {
+            return products.update(getData.id, editObj)
+              .then(updateData => {
+                return products.delete(updateData.id)
+                  .then(deletedData => {
+                    return products.get(deletedData.id).
+                      then(emptyData => {
+                        expect(!!deletedData.id).toEqual(true);
+                        expect(emptyData).toEqual(null);
+                      })
+                    });
+                  });
+              });
+          });
+      }).catch(e => console.error('ERR', e));
+  });
+});
